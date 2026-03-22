3aa5c480bb744641b66d84544676ec1a56cad2ce
PabloCdL <angelcoyoy07@gmail.com>
2026-03-22 12:55:56 -0600

    Definicion de mis variables

--- Proyecto Streaming/Program.cs
+++ Proyecto Streaming/Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿//Definicion de mis variables
+//VAriables de operaciones
+string tipocontenido;
+int duracion;
+string clasificacion;
+int hora;
+string nivelP;
+int opcion;
+//Variables Contadoritas :D
+int publicado = 0;
+int revision = 0;
+int totalevaluados = 0;
+int impactobajo = 0;
+int impactomedio = 0;
+int impactoalto = 0;
+int rechazados = 0;
+//Variables Auxiliares
+bool valido = true;
+string razon = "";
+string impacto = "";
+int num;
+double porcentaje;
